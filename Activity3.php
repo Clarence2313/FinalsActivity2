c26806b149afbf2bb706297674ca8c2d07c7944e
Clarence2313 <clarence.bretana213@gmail.com>
2026-04-27 23:16:00 +0800

    Create Activity3.php

---
+++ Activity3.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Clarence P. Bretaña";
+$age = 20;
+$course = "BSIT";
+$section = "1X";
+
+echo "Name: " , $name , "<br>";
+echo "Age: " , $age , "<br>";
+echo "Course: " , $course , "<br>";
+echo "Section: " , $section , "<br>";
+
+?>
