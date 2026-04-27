ce9eb22fa1e2a56f52b2cce62cfb0bfdaec056b7
Clarence2313 <clarence.bretana213@gmail.com>
2026-04-27 23:25:29 +0800

    Create Activity5.php

---
+++ Activity5.php
@@ -0,0 +1,9 @@
+<?php
+$num1 = 25;
+$num2 = 16;
+
+echo "Addition: " . ($num1 + $num2) . "<br>";
+echo "Subtraction: " . ($num1 - $num2) . "<br>";
+echo "Multiplication: " . ($num1 * $num2) . "<br>";
+echo "Division: " . ($num1 / $num2);
+?>
